9147aa060c0b8665f0c369dc345f37c47463709c
weimeilin79 <weimeilin@gmail.com>
2023-04-15 08:45:55 -0400

    Trying different Redpanda for testing CI

--- src/test/java/org/acme/TestResource.java
+++ src/test/java/org/acme/TestResource.java
@@ -14,16 +14,12 @@ public class TestResource implements QuarkusTestResourceLifecycleManager {
     static final RedpandaContainer redpanda = new RedpandaContainer("docker.redpanda.com/vectorized/redpanda:latest"){
         protected void containerIsStarting(InspectContainerResponse containerInfo) {
             String command = "#!/bin/bash\n";
-            command = command + "/usr/bin/rpk redpanda start --mode dev-container --overprovisioned  --smp=300m --memory=600M ";
+            command = command + "/usr/bin/rpk redpanda start --mode dev-container --overprovisioned --smp=1 --memory=2G ";
             command = command + "--kafka-addr PLAINTEXT://0.0.0.0:29092,OUTSIDE://0.0.0.0:9092 ";
             command = command + "--advertise-kafka-addr PLAINTEXT://127.0.0.1:29092,OUTSIDE://" + this.getHost() + ":" + this.getMappedPort(9092);
             this.copyFileToContainer(Transferable.of(command, 511), "/testcontainers_start.sh");
         }
-    }
-      .withCreateContainerCmdModifier(cmd -> {
-          cmd.getHostConfig()
-            .withCpuCount(2l);
-      });
+    };
 
 
     
